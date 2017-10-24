ac194e943f0c04e12fbd9f9c580bead2426b207e
Unknown <alfian853@gmail.com>
2017-10-24 21:32:44 +0700

    Auto stash before merge of "master" and "origin/master"
    sudah fix

--- soal4.c
+++ soal4.c
@@ -8,23 +8,31 @@
 int now, hasil=1, bil, status=1;
 
 void* faktorial(void *arg){
-    for(int i=1;i<= bil;i++){
+    int bil = (int*)arg;
+    int hasil=1;
+    for(int i=2;i<= bil;i++){
         hasil=i*hasil;
     }
-    status++;
     printf("%d!= %d\n",bil,hasil);
 }
 
-int main(int argc, int *argv[])
-{
-    pthread_t tid[argc];
 
-    for(int i=1;i<=argc;i++){
-        now = i;
-        printf("------->%c\n", (int)*argv[i]);
-        bil = (int)*argv[i]-(int)'0';
-        pthread_create(&(tid[i]),NULL,&faktorial,NULL);
-        while(status-1!=i){}
+int main(int argc,char *argv[]){
+
+    pthread_t tid[argc];
+    int bil;
+    for(int i=1;i<argc;i++){
+
+        int j=strlen(argv[i])-1;
+        bil=0;
+        while(j>-1){
+            bil*=10;
+            bil+=(argv[i][j]-48);
+            //printf("x: %d ",argv[i][j]-'0');
+            --j;
+        }
+        pthread_create(&(tid[i]),NULL,&faktorial,(void*)bil);
+        pthread_join(tid[i],NULL);
     }
 
     return 0;
