e9191407999c725f1bdb6417e4fda0f64a5a33f1
Unknown <alfian853@gmail.com>
2017-10-24 20:25:45 +0700

    sudah fix

--- soal2.c
+++ soal2.c
@@ -11,11 +11,12 @@ pthread_t tid2;
 status 0 pemain1 input trap
 status 1 pemain2 tebak trap lalu pemain2 input trap
 status 2 pemain1 tebak trap
+status 3 game over
 
 */
 int status;
 int nomor,jumlah;
-int skor1=0,skor2=0;
+int skor1=0,skor2=0,stok1=16,stok2=16;
 char ladang1[17];
 char ladang2[17];
 
@@ -25,13 +26,19 @@ void* pemain1(void *arg){
         printf("skor pemain1 : %d\nskor pemain2 : %d\n",skor1,skor2);
         status=0;
         printf("pemain 1, [jumlah trap] [nomor ladang sebanyak jumlah trap dipisahkan spasi]\n> ");
+
         scanf("%d",&jumlah);
+
         while(jumlah--){
             scanf("%d",&nomor);
+            //asumsi field yang sudah terisi,bisa di isi lagi,sehingga tidak berpengaruh
+            if(ladang1[nomor]=='o')stok1--;
             ladang1[nomor]='x';
         }
         status=1;
-        while(status==1);
+        while(status==1||status==3){
+            if(status==3)return NULL;
+        }
         printf("pemain 1, [nomor ladang tebak]\n> ");
 
         scanf("%d",&nomor);
@@ -43,6 +50,7 @@ void* pemain1(void *arg){
             printf("tebakan pemain1 salah!, poin pemain2 ditambah 1\n");
             ++skor2;
         }
+        if(skor1==10 || skor2 == 10 || stok1==0 || stok2==0){status=3;return NULL;}
     }
     return NULL;
 }
@@ -51,7 +59,9 @@ void* pemain1(void *arg){
 void* pemain2(void *arg){
     while(1){
         status=0;
-        while(status==0);
+        while(status==0||status==3){
+            if(status==3)return NULL;
+        }
         printf("pemain 2, [nomor ladang tebak]\n> ");
         scanf("%d",&nomor);
         if(ladang1[nomor]=='o'){
@@ -62,12 +72,14 @@ void* pemain2(void *arg){
             printf("tebakan pemain2 salah!, poin pemain1 ditambah 1\n");
             ++skor1;
         }
+        if(skor1==10 || skor2 == 10 || stok1==0 || stok2==0){status=3;return NULL;}
 
         printf("skor pemain1 : %d\nskor pemain2 : %d\n",skor1,skor2);
         printf("pemain 2, [jumlah trap] [nomor ladang sebanyak jumlah trap dipisahkan spasi]\n> ");
         scanf("%d",&jumlah);
         while(jumlah--){
             scanf("%d",&nomor);
+            if(ladang2[nomor]=='o')stok2--;
             ladang2[nomor]='x';
         }
         status=2;
